c3ca693c9ae7f12bcf49536bb406243c25e8e3aa
unknown <zhanghao@xaguanggu.com>
2020-01-22 12:23:22 +0800

    C#基础类型扩展方法，可空DateTime增加ToShortString等方法

---
+++ CSpBasicTypeExtension.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace CSpBasicTypeExtension
+{
+    public static class DateTimeExtension
+    {
+        /// <summary>
+        /// 可空日期转短日期字符串
+        /// </summary>
+        /// <returns></returns>
+        public static string ToShortTimeString(this DateTime? date)
+        {
+            if (date == null)
+            {
+                return "";
+            }
+            else
+            {
+                return date.Value.ToShortDateString();
+            }
+        }
+    }
+    public static class StringExtension
+    {
+        /// <summary>
+        /// 字符串转时间
+        /// </summary>
+        /// <param name="src"></param>
+        /// <returns></returns>
+        public static DateTime? ToDateTime(this string src)
+        {
+            if (DateTime.TryParse(src, out DateTime date))
+            {
+                return date;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+    public static class ObjectExtension
+    {
+        /// <summary>
+        /// obj转可空int类型，obj为空，返回空，obj转int成功，正常返回，否则返回null
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static int? objToInt(this object obj)
+        {
+            if (obj == null)
+            {
+                return null;
+            }
+            else
+            {
+                if (int.TryParse(obj.ToString(), out int res))
+                {
+                    return res;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+        /// <summary>
+        /// obj转可空double类型，obj为空，返回空，obj转double成功，正常返回，否则返回null
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static double? objToDouble(this object obj)
+        {
+            if (obj == null)
+            {
+                return null;
+            }
+            else
+            {
+                if (double.TryParse(obj.ToString(), out double res))
+                {
+                    return res;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
